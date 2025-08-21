f6875e62ab4b4705789f53c814cb17a10ca9f6f7
Melany Quintanilla <022200795b@uandina.edu.pe>
2025-08-20 23:27:11 -0500

    version reto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// Archivo: lib/main.dart
+
+import 'package:design_patron/view/counter_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../viewmodel/counter_viewmodel.dart';
@@ -19,57 +22,6 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: CounterView(),
-    );
-  }
-}
-
-class CounterView extends StatelessWidget {
-  const CounterView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final counterViewModel = Provider.of<CounterViewModel>(context);
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Contador con MVVM')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Contador: ${counterViewModel.count}",
-              style: const TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: counterViewModel.increment,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.lightBlue,
-                  ),
-                  child: const Icon(Icons.add),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: counterViewModel.decrement,
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                  child: const Icon(Icons.remove),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: counterViewModel.reset,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.lightGreen,
-                  ),
-                  child: const Icon(Icons.refresh),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
